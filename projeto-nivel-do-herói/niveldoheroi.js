ab61bbb448f6f5e5a7f6adf05a629c8df8214fab
felipearaujo <felipearaujocst@gmail.com>
2025-07-04 12:28:52 -0300

    finalizado

--- projeto-nivel-do-herói/niveldoheroi.js
+++ projeto-nivel-do-herói/niveldoheroi.js
@@ -7,15 +7,30 @@ function calcularXp(){
 
     if(xp < 1000){
         nivel = "Ferro"
-    } else if (xp >= 1000 && xp < 2000){
+    }else if (xp >= 1000 && xp < 2000){
         nivel = "Bronze"
+    }else if (xp >= 2000 && xp < 5000){
+        nivel = "Prata"
+    }else if (xp >= 5000 && xp < 7000){
+        nivel = "Ouro"
+    }else if (xp >= 7000 && xp < 8000){
+        nivel = "Platina"
+    }else if (xp >= 8000 && xp < 9000){
+        nivel = "Ascendente"
+    }else if (xp >= 9000 && xp < 10000){
+        nivel = "Imortal"
+    }else if (xp >= 10000){
+        nivel = "Radiante"
     }
 
-    const novaDiv = document.createElement("div")
-    novaDiv.classList.add("resultado")
-    novaDiv.innerHTML =`Seu herói se chama <strong>${heroName}</strong> e seu nível é <strong>${nivel}</strong>!`
-    
-    const container = document.querySelector(".container")
-    container.appendChild(novaDiv)
+    let resultadoDiv = document.querySelector(".resultado")
+
+    if(!resultadoDiv){
+        resultadoDiv = document.createElement("div")
+        resultadoDiv.classList.add("resultado")
+        document.querySelector(".container").appendChild(resultadoDiv)
+    }
+
+    resultadoDiv.innerHTML = `Seu herói se chama <strong>${heroName}</strong> e seu nível é <strong>${nivel}</strong>!`
 }
 
