400cc9b536f88b234d4ca96d562df8e6e404fc55
felipearaujo <felipearaujocst@gmail.com>
2025-07-06 20:59:41 -0300

    exexrcicios

--- curso-javascript/aula3.js
+++ curso-javascript/aula3.js
@@ -16,3 +16,82 @@ console.log(numAoQuadrado) */
 const incremento =  incrementarJuros(100, 10)
 
 console.log(incremento) */
+
+/* function calcularJuros(){
+
+}
+
+function main() {
+    console.log('Programa principal')
+    calcularJuros()
+}
+
+main() */
+
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura, 2)
+}
+
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return (`${imc}abaixo do peso`)
+    } else if (imc >= 18.5 && imc < 25) {
+        return (`peso normal`)
+    } else if (imc >= 25 && imc < 30) {
+        return (`acima do peso`)
+    } else if (imc >= 30 && imc < 40) {
+        return (`Obeso`)
+    } else {
+        return (`obesidade grave`)
+    }
+}
+
+/* (function () {
+    const peso = 60
+    const altura = 1.70
+
+    const imc = calcularImc(peso, altura)
+    console.log(classificarImc(imc))
+})() */
+
+// function escrevaNome(nome){
+//     return nome 
+// }
+
+// function verificarIdade(idade){
+//     if(idade >= 18){
+//         console.log(escrevaNome("Raul") + " é Maior de idade")
+//     } else {
+//         console.log("Menor Idade")
+//     }
+// }
+
+// verificarIdade(18)
+
+function aplicarDesconto(valor, desconto){
+    return (valor - ((desconto * valor) / 100))
+}
+
+function aplicarJuros(valor, juros, parcela){
+    return (valor + ((juros * valor) / 100))/parcela
+}
+
+const valorCamisa = 54
+const formaPagamento = 4
+const parcela = 3
+
+
+if (formaPagamento === 1){
+    console.log(aplicarDesconto(valorCamisa, 10))
+} else if (formaPagamento === 2){
+    console.log(aplicarDesconto(valorCamisa, 15))
+} else if (formaPagamento === 3){
+    console.log(valorCamisa)
+} else {
+    console.log(aplicarJuros(valorCamisa, 10, parcela))
+}
+
+
+
+
+
