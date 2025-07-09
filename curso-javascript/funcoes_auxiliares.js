437489a5cd479480cafbe248fec0fb7885f3925c
felipearaujo <felipearaujocst@gmail.com>
2025-07-09 12:22:18 -0300

    aulas 8

--- curso-javascript/funcoes_auxiliares.js
+++ curso-javascript/funcoes_auxiliares.js
@@ -1,5 +1,12 @@
+const entradas = [10, 5, 50, 10, 98, 23, 44, 99, 100, 7, 8, 120]
+let i = 0;
+
+
+
 function gets(){
-    return 10
+    const valor = entradas[i]
+    i++
+    return valor
 }
 
 function print(texto){
--- curso-javascript/main.js
+++ curso-javascript/main.js
@@ -1,4 +1,46 @@
-const funcoes = require('./funcoes_auxiliares');
+// const {gets, print}= require('./funcoes_auxiliares');
 
-console.log(funcoes.gets())
+// const numerosSorteados = [];
+
+// for (let index = 0; index < 5; index++) {
+//     const numerosSorteado = gets();
+//     numerosSorteados.push(numerosSorteado)
+// }
+
+// print(numerosSorteados)
+
+// let maiorNumero = 0
+
+// for (let i = 0; i < numerosSorteados.length; i++) {
+//     const numerosSorteado = numerosSorteados[i]
+//     if(numerosSorteado > maiorNumero){
+//         maiorNumero = numerosSorteado;
+//     }
+    
+// }
+
+
+// print(maiorNumero)
+
+const {gets, print}= require('./funcoes_auxiliares');
+
+const qntDeAlunos = gets();
+
+let maiorNumeroEncontrado = 0;
+
+for (let index = 0; index < qntDeAlunos; index++) {
+    const numerosSorteado = gets();
+    if(numerosSorteado > maiorNumeroEncontrado){
+        maiorNumeroEncontrado = numerosSorteado 
+    }
+}
+
+
+print(maiorNumeroEncontrado)
+
+
+
+
+
+
 
