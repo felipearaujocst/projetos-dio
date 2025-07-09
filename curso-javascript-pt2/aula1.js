1ff38a2dd57e17d711e10d0961895ca83837cffc
felipearaujo <felipearaujocst@gmail.com>
2025-07-09 20:23:38 -0300

    json

---
+++ curso-javascript-pt2/aula1.js
@@ -0,0 +1,27 @@
+let invoice = {
+    name: "Felipe",
+    age: 30,
+    products: {
+        0: ["mouse 2xwm", 29.90],
+        1: ["Teclado mecânico", 129.99],
+        2: ["Monitor", 899.99],
+        3: ["TV 100 pol", 10000.90]
+    }
+}
+
+
+generateInvoice(invoice)
+
+function generateInvoice(invoice) {
+    console.log(`O comprador é ${invoice.name}`)
+    console.log(`A idade é ${invoice.age}`)
+    console.log("--------------------------")
+    for (let i in invoice.products) {
+        let [productName, ProductPrice] = invoice.products[i]
+        console.log(` - ${productName}: R$ ${ProductPrice}`)
+    }
+}
+
+
+
+
